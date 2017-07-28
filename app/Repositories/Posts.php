24e7ebc473bc05e55e93f288bb0b8bbab4849e70
Mironenko Dmitry <mironenko@gorodok.net>
2017-07-28 16:10:36 +0700

    + More powerful post's repository.

--- app/Repositories/Posts.php
+++ app/Repositories/Posts.php
@@ -8,16 +8,40 @@ use App\Redis;
 class Posts
 {
     protected $redis;
+    protected $allBulk;
+    protected $count;
 
     public function __construct(Redis $redis) {
         $this->redis = $redis;
+        $this->allBulk = false;
+        $this->count = 0;
     }
 
     public function all() {
-        return Post::all();
+        if (!$this->allBulk) {
+            $posts = Post::all()->get();
+            $this->count = $posts->count();
+            foreach ($posts as $post) {
+                $this->redis->append('post_'.$post->id, $post);
+            }
+            $this->allBulk = true;
+        } else {
+            $keys = array_map(function($post) { return 'post_'.$post->id; }, range(1, $this->count));
+            $posts = $this->redis->getMultiple($keys);
+        }
+
+        return $posts;
     }
 
     public function find($id) {
+        $key = 'post_' . $id;
+        if (!$this->redis->exists($key)){
+            $post = Post::find($id);
+            $this->redis->append($key, $post);
+        } else {
+            $post = $this->redis->get($key);
+        }
 
+        return $post;
     }
 }
